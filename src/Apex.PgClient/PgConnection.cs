15c54c59a76b3664cae001643d71bc6c7594e031
Sébastien Ros <1165805+sebastienros@users.noreply.github.com>
2026-08-15 23:02:36 -0700

    Ignore connection errors during disposal

--- src/Apex.PgClient/PgConnection.cs
+++ src/Apex.PgClient/PgConnection.cs
@@ -337,7 +337,14 @@ public sealed class PgConnection : ISqlConnection
         finally
         {
             await _scheduler.DisposeAsync().ConfigureAwait(false);
-            await _pipeWriter.CompleteAsync().ConfigureAwait(false);
+            try
+            {
+                await _pipeWriter.CompleteAsync().ConfigureAwait(false);
+            }
+            catch (Exception exception) when (IsFatalConnectionError(exception))
+            {
+            }
+
             await _reader.CompleteAsync().ConfigureAwait(false);
             await _stream.DisposeAsync().ConfigureAwait(false);
             _socket.Dispose();
